6c00e2d68a0ae10603436800df4791dfa863d3cb
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-04-29 13:22:16 +0200

    feat: added interactive map for contact page

---
+++ components/ContactMap.js
@@ -0,0 +1,32 @@
+import ReactMapGL, { Marker } from "react-map-gl";
+import { useState } from "react";
+
+const ContactMap = (props) => {
+  const [viewport, setViewport] = useState({
+    latitude: props.lat,
+    longitude: props.long,
+    width: "100%",
+    height: "100%",
+    zoom: 10,
+  });
+
+  return (
+    <div className={props.map}>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={
+          "pk.eyJ1IjoibW9udHJvbmFzIiwiYSI6ImNrbnNtZjVoczEwY20ydm8xaThtMnh2MjcifQ.0KHG1t-eIqrmTkZlIDbULA"
+        }
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      >
+        <Marker latitude={props.lat} longitude={props.long}>
+          <div className={props.marker}></div>
+        </Marker>
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default ContactMap;
