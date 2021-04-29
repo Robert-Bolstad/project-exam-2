a19985e01080d7d667d6b93aa08036efea97a588
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-04-29 13:27:52 +0200

    feat: added head component

---
+++ components/layout/Head.js
@@ -0,0 +1,18 @@
+import NextHead from "next/head";
+
+export default function Head({ title = "" }) {
+  return (
+    <NextHead>
+      <title>
+        {title}
+        {title ? " | " : ""}Holidaze
+      </title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans&amp;family=Poppins&amp;display=swap"
+        rel="stylesheet"
+      />
+    </NextHead>
+  );
+}
