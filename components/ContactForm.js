ef818876038562503680072fcd670267af7ddeab
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-03 23:12:45 +0200

    feat: adde code for post request

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,9 +1,14 @@
 import { useForm } from "react-hook-form";
+import axios from "axios";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useState } from "react";
+import { BASE_URL } from "../settings/api";
+import Image from "next/image";
 
 const ContactForm = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [success, setSuccess] = useState("");
   const schema = yup.object().shape({
     firstName: yup
       .string()
@@ -31,130 +36,151 @@ const ContactForm = () => {
       .min(10, "* The message must be at least 10 characters"),
   });
 
+  function successHtml() {
+    return (
+      <>
+        <p>Message sendt</p>
+        <div className="contact-form__check-img">
+          <Image src="/check.svg" width="12" height="12" alt="check icon" />
+        </div>
+      </>
+    );
+  }
+
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(schema),
   });
 
-  const [passValidation, setPassValidation] = useState();
+  const onSubmit = async (data) => {
+    setSubmitting(true);
 
-  function onSubmit(data) {
-    if (data) {
-      setPassValidation("Validation Passed");
+    try {
+      const response = await axios.post(BASE_URL + "/messages", data);
+      console.log(data);
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      setSubmitting(false);
+      setSuccess(successHtml());
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="contact-form">
-      <div>{passValidation}</div>
-      <div className="contact-form__wrapper">
-        <div className="contact-form__group">
-          <label
-            className="contact-form__label"
-            htmlFor="contact-form__input--name"
-          >
-            First name
-          </label>
-          <input
-            ref={register}
-            className="contact-form__input contact-form__input--name"
-            type="text"
-            name="firstName"
-            id="contact-form__input--name"
-          />
-          {errors.firstName && (
-            <span className="form__error-message">
-              {errors.firstName.message}
-            </span>
-          )}
+      <div className="contact-form__success">{success}</div>
+      <fieldset className="contact-form__fieldset" disabled={submitting}>
+        <div className="contact-form__wrapper">
+          <div className="contact-form__group">
+            <label
+              className="contact-form__label"
+              htmlFor="contact-form__input--name"
+            >
+              First name
+            </label>
+            <input
+              ref={register}
+              className="contact-form__input contact-form__input--name"
+              type="text"
+              name="firstName"
+              id="contact-form__input--name"
+            />
+            {errors.firstName && (
+              <span className="form__error-message">
+                {errors.firstName.message}
+              </span>
+            )}
+          </div>
+
+          <div className="contact-form__group">
+            <label
+              className="contact-form__label"
+              htmlFor="contact-form__input--last-name"
+            >
+              Last name
+            </label>
+            <input
+              ref={register}
+              className="contact-form__input contact-form__input--last-name"
+              type="text"
+              name="lastName"
+              id="contact-form__input--last-name"
+            />
+            {errors.lastName && (
+              <span className="form__error-message">
+                {errors.lastName.message}
+              </span>
+            )}
+          </div>
+
+          <div className="contact-form__group">
+            <label
+              className="contact-form__label"
+              htmlFor="contact-form__input--email"
+            >
+              Email
+            </label>
+            <input
+              className="contact-form__input contact-form__input--email"
+              type="text"
+              name="email"
+              ref={register}
+              id="contact-form__input--email"
+            />
+            {errors.email && (
+              <span className="form__error-message">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+
+          <div className="contact-form__group contact-form__subject">
+            <label
+              className="contact-form__label"
+              htmlFor="contact-form__input--subject"
+            >
+              Subject
+            </label>
+            <input
+              className="contact-form__input contact-form__input--subject"
+              id="contact-form__input--subject"
+              type="text"
+              name="subject"
+              ref={register}
+            />
+            {errors.subject && (
+              <span className="form__error-message">
+                {errors.subject.message}
+              </span>
+            )}
+          </div>
+        </div>
+        <div className="contact-form__wrapper">
+          <div className="contact-form__group">
+            <label
+              className="contact-form__label"
+              htmlFor="contact-form__input--textbox"
+            >
+              Message
+            </label>
+            <textarea
+              className="contact-form__textbox"
+              name="message"
+              ref={register}
+              type="text"
+              id="contact-form__input--textbox"
+            />
+            {errors.message && (
+              <span className="form__error-message">
+                {errors.message.message}
+              </span>
+            )}
+          </div>
         </div>
 
         <div className="contact-form__group">
-          <label
-            className="contact-form__label"
-            htmlFor="contact-form__input--last-name"
-          >
-            Last name
-          </label>
-          <input
-            ref={register}
-            className="contact-form__input contact-form__input--last-name"
-            type="text"
-            name="lastName"
-            id="contact-form__input--last-name"
-          />
-          {errors.lastName && (
-            <span className="form__error-message">
-              {errors.lastName.message}
-            </span>
-          )}
+          <button className="contact-form__submit">Submit</button>
         </div>
-
-        <div className="contact-form__group">
-          <label
-            className="contact-form__label"
-            htmlFor="contact-form__input--email"
-          >
-            Email
-          </label>
-          <input
-            className="contact-form__input contact-form__input--email"
-            type="text"
-            name="email"
-            ref={register}
-            id="contact-form__input--email"
-          />
-          {errors.email && (
-            <span className="form__error-message">{errors.email.message}</span>
-          )}
-        </div>
-
-        <div className="contact-form__group contact-form__subject">
-          <label
-            className="contact-form__label"
-            htmlFor="contact-form__input--subject"
-          >
-            Subject
-          </label>
-          <input
-            className="contact-form__input contact-form__input--subject"
-            id="contact-form__input--subject"
-            type="text"
-            name="subject"
-            ref={register}
-          />
-          {errors.subject && (
-            <span className="form__error-message">
-              {errors.subject.message}
-            </span>
-          )}
-        </div>
-      </div>
-      <div className="contact-form__wrapper">
-        <div className="contact-form__group">
-          <label
-            className="contact-form__label"
-            htmlFor="contact-form__input--textbox"
-          >
-            Message
-          </label>
-          <textarea
-            className="contact-form__textbox"
-            name="message"
-            ref={register}
-            type="text"
-            id="contact-form__input--textbox"
-          />
-          {errors.message && (
-            <span className="form__error-message">
-              {errors.message.message}
-            </span>
-          )}
-        </div>
-      </div>
-
-      <div className="contact-form__group">
-        <button className="contact-form__submit">Submit</button>
-      </div>
+      </fieldset>
     </form>
   );
 };
