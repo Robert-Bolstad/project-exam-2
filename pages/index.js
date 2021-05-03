02aa39b6ebba742ddff64ab75262724749316512
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-03 23:17:20 +0200

    feat: added code for featured hotels section

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,33 @@ import Card from "../components/Card";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 import SearchBox from "../components/SearchBox";
+import { BASE_URL } from "../settings/api";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ results }) {
+  const featuredHotels = () => {
+    const hotels = results.filter((hotel) => hotel.category === "hotel");
+    const featured = hotels.filter((featured) => featured.featured === true);
+    return featured;
+  };
+  const featuredBB = () => {
+    const BB = results.filter(
+      (hotel) => hotel.category === "bed_and_breakfast"
+    );
+    const featured = BB.filter((featured) => featured.featured === true);
+    return featured;
+  };
+  const featuredGuestHouses = () => {
+    const houses = results.filter((hotel) => hotel.category === "guesthouse");
+    const featured = houses.filter((featured) => featured.featured === true);
+    return featured;
+  };
+
   return (
     <Layout>
       <Head title="Home" />
       <main>
-        <SearchBox />
+        <SearchBox data={results} />
         <section className="featured">
           <h2 className="featured__heading">Popular Accommodations</h2>
 
@@ -22,13 +42,17 @@ export default function Home() {
               <h3 className="featured__subheading">Hotels</h3>
             </div>
             <div className="featured__cards">
-              <Card />
-              <Card />
-              <Card />
+              {featuredHotels().map((hotel) => {
+                return <Card key={hotel.id} hotel={hotel} />;
+              })}
             </div>
             <Link href="/">
               <a className="featured__link">
-                See all(<span className="featured__link--number">10</span>)
+                See all(
+                <span className="featured__link--number">
+                  {featuredHotels().length}
+                </span>
+                )
               </a>
             </Link>
           </div>
@@ -39,13 +63,17 @@ export default function Home() {
               <h3 className="featured__subheading">Bed &amp; Breakfast</h3>
             </div>
             <div className="featured__cards">
-              <Card />
-              <Card />
-              <Card />
+              {featuredBB().map((hotel) => {
+                return <Card key={hotel.id} hotel={hotel} />;
+              })}
             </div>
             <Link href="/">
               <a className="featured__link">
-                See all(<span className="featured__link--number">10</span>)
+                See all(
+                <span className="featured__link--number">
+                  {featuredBB().length}
+                </span>
+                )
               </a>
             </Link>
           </div>
@@ -61,13 +89,17 @@ export default function Home() {
               <h3 className="featured__subheading">Guest Houses</h3>
             </div>
             <div className="featured__cards">
-              <Card />
-              <Card />
-              <Card />
+              {featuredGuestHouses().map((hotel) => {
+                return <Card key={hotel.id} hotel={hotel} />;
+              })}
             </div>
             <Link href="/">
               <a className="featured__link">
-                See all(<span className="featured__link--number">10</span>)
+                See all(
+                <span className="featured__link--number">
+                  {featuredGuestHouses().length}
+                </span>
+                )
               </a>
             </Link>
           </div>
@@ -77,4 +109,23 @@ export default function Home() {
       <Footer />
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  let results = [];
+
+  try {
+    const response = await axios.get(BASE_URL + "/establishments");
+
+    results = response.data;
+    console.log(results);
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    props: {
+      results: results,
+    },
+  };
 }
