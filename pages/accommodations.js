3c4b030502a0fcd7ac98260486846b884e3fd043
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-08 18:11:44 +0200

    feat: updated and added new code

--- pages/accommodations.js
+++ pages/accommodations.js
@@ -8,6 +8,7 @@ import axios from "axios";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { useState } from "react";
 import ResultMap from "../components/ResultMap";
+import FilterList from "../components/FilterList";
 
 export default function Accommodations(props) {
   function toggleMap() {
@@ -23,55 +24,7 @@ export default function Accommodations(props) {
       <main className="accommodations">
         <section className="accommodations__section accommodations__section--one">
           <h1 className="accommodations__heading">Accomodations in bergen</h1>
-          <form className="filter-list">
-            <div className="filter-list__wrapper">
-              <div className="filter-list__group">
-                <label className="filter-list__label" htmlFor="">
-                  Max Price
-                </label>
-                <input
-                  className="filter-list__input"
-                  placeholder="0$"
-                  type="number"
-                />
-              </div>
-            </div>
-
-            <div className="filter-list__wrapper">
-              <div className="filter-list__group">
-                <label
-                  className="filter-list__label"
-                  htmlFor="filter-list__type"
-                >
-                  Accommodation
-                </label>
-                <select className="filter-list__type" name="type" type="text">
-                  <option className="filter-list__option" value="all">
-                    All
-                  </option>
-                  <option className="filter-list__option" value="hotel">
-                    Hotels
-                  </option>
-                  <option className="filter-list__option" value="bb">
-                    B&#38;Bs
-                  </option>
-                  <option className="filter-list__option" value="guesthouse">
-                    Guesthouses
-                  </option>
-                </select>
-              </div>
-            </div>
-
-            <button className="filter-list__search">
-              <Image
-                src="/search.svg"
-                width="33"
-                height="33"
-                alt="hotel icon"
-              />
-              Search
-            </button>
-          </form>
+          <FilterList />
         </section>
         <section className="accommodations__section accommodations__section--two">
           <div className="accommodations__result-info">
@@ -93,42 +46,51 @@ export default function Accommodations(props) {
             </button>
           </div>
           <div className="accommodations__result">
-            {props.results.map((card) => {
-              return (
-                <div className="accommodation-card" key={card.id}>
-                  <div className="accommodation-card__img">
-                    <Image
-                      src={BASE_URL + card.image.url}
-                      width="auto"
-                      height="180px"
-                      alt="image of establishment"
-                    />
-                  </div>
-                  <div className="accommodation-card__inner">
-                    <div>
-                      <h4 className="accommodation-card__name">{card.name}</h4>
-                      <p className="accommodation-card__description">
-                        {card.description}
-                      </p>
-                      <div className="accommodation-card__link-btn">
-                        <Link href={`/detail/${card.id}`}>
-                          <a className="accommodation-card__link">Visit page</a>
-                        </Link>
+            <div className="accommodations__card-wrapper">
+              {props.results.map((card) => {
+                return (
+                  <div className="accommodation-card" key={card.id}>
+                    <div className="accommodation-card__img">
+                      <Image
+                        src={BASE_URL + card.image.url}
+                        width="auto"
+                        height="auto"
+                        alt="image of establishment"
+                      />
+                    </div>
+                    <div className="accommodation-card__inner">
+                      <div className="accommodation-card__info">
+                        <h4 className="accommodation-card__name">
+                          {card.name}
+                        </h4>
+                        <p className="accommodation-card__description">
+                          {card.description.slice(0, 100)}
+                        </p>
+                        <div className="accommodation-card__link-btn">
+                          <Link href={`/detail/${card.id}`}>
+                            <a className="accommodation-card__link">
+                              Visit page
+                            </a>
+                          </Link>
+                        </div>
+                      </div>
+                      <div className="accommodation-card__price-wrapper">
+                        <div className="accommodation-card__price-text accommodation-card__price-text--highlight">
+                          From
+                        </div>
+                        <div className="accommodation-card__price">
+                          ${card.price}
+                        </div>
+                        <div className="accommodation-card__price-text">
+                          per night
+                        </div>
                       </div>
                     </div>
-                    <div className="accommodation-card__price-wrapper">
-                      <div className="accommodation-card__price-text">From</div>
-                      <div className="accommodation-card__price">
-                        ${card.price}
-                      </div>
-                      <div className="accommodation-card__price-text">
-                        per night
-                      </div>
-                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
+
             <ResultMap props={props} />
             <div className="map__overlay">
               <button className="map__close" onClick={toggleMap}>
@@ -137,8 +99,8 @@ export default function Accommodations(props) {
             </div>
           </div>
         </section>
-        <Footer />
       </main>
+      <Footer />
     </Layout>
   );
 }
