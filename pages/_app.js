ec270e1cc6be96b07979a4cb311467f593ffc16d
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-08 18:10:18 +0200

    feat: imported enquiry provider

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@ import "../sass/main.scss";
 import "react-datepicker/dist/react-datepicker.css";
 
 import { AuthProvider } from "../context/AuthContext";
+import { EnquiryProvider } from "../context/EnquiryContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <EnquiryProvider>
+        <Component {...pageProps} />
+      </EnquiryProvider>
     </AuthProvider>
   );
 }
