c2b99644dc93b2a9c587fdb6ad4d0b873b668467
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-08 18:08:15 +0200

    feat: updates message component and added new code

--- components/Messages.js
+++ components/Messages.js
@@ -12,63 +12,64 @@ const Messages = ({ data }) => {
   };
 
   function openMesssage(element, id) {
-    const message = element.parentElement.querySelector(".messages__message");
-    message.classList.toggle("messages__message--open");
-    element.classList.remove("messages__message-btn--unread");
-    element.classList.add("messages__message-btn--read");
+    const message = element.parentElement.querySelector(".Messages__message");
+    const btnImg = element.querySelector(".Messages__btn-img");
+    message.classList.toggle("Messages__message--open");
+    element.classList.remove("Messages__btn--unread");
+    element.classList.add("Messages__btn--read");
+    btnImg.classList.toggle("Messages__btn-img--open");
     update(id);
   }
 
   function readMessage(data) {
     if (data === true) {
-      return "messages__message-btn messages__message-btn--read";
+      return "Messages__btn Messages__btn--read";
     } else {
-      return "messages__message-btn messages__message-btn--unread";
+      return "Messages__btn Messages__btn--unread";
     }
   }
   return (
     <>
-      <div className="messages">
-        <h1 className="messages__heading">Lastest Messages</h1>
+      <div className="Messages">
+        <h1 className="Messages__heading">Lastest Messages</h1>
         {data.map((data) => {
           return (
-            <div key={data.id} className="messages__wrapper">
+            <div key={data.id} className="Messages__wrapper">
               <button
                 onClick={(event) => openMesssage(event.currentTarget, data.id)}
                 className={readMessage(data.read)}
               >
-                <span className="messages__info">
-                  <span className="messages__info--bold">From:</span>{" "}
-                  {data.firstName + " " + data.lastName}
+                <span className="Messages__btn-text">
+                  {"Subject: " + data.subject}
                 </span>
-                <span className="messages__info">
-                  <span className="messages__info--bold">Subject:</span>{" "}
-                  {data.subject}
-                </span>
-                <span className="messages__info">
-                  <span className="messages__info--bold">message:</span>{" "}
-                  {data.message}
-                </span>
+                <img
+                  src="/arrow.svg"
+                  alt="arrow-icon"
+                  className="Messages__btn-img"
+                />
               </button>
-              <div className="messages__message">
-                <div className="messages__message-inner">
-                  <p className="messages__message-info">
-                    <span className="messages__message-info--bold">
-                      Sender:
+              <div className="Messages__message">
+                <div className="Messages__message-inner">
+                  <p className="Messages__message-info">
+                    Sender:
+                    <span className="Messages__message-detail">
+                      {data.email}
                     </span>
-                    {data.email}
                   </p>
-                  <p className="messages__message-info">
-                    <span className="messages__message-info--bold">From:</span>
-                    {data.firstName + " " + data.lastName}
-                  </p>
-                  <p className="messages__message-info">
-                    <span className="messages__message-info--bold">
-                      Subject:
+                  <p className="Messages__message-info">
+                    From:
+                    <span className="Messages__message-detail">
+                      {" " + data.firstName + " " + data.lastName}
                     </span>
-                    {data.subject}
                   </p>
-                  <p className="messages__message-info">{data.message}</p>
+                  <p className="Messages__message-info">
+                    Subject:
+                    <span className="Messages__message-detail">
+                      {" " + data.subject}
+                    </span>
+                  </p>
+
+                  <p className="Messages__message-content">{data.message}</p>
                 </div>
               </div>
             </div>
