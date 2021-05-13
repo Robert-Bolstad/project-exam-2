60eb249f781f6f68904696029e505f700b528590
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-13 19:47:00 +0200

    feat: added functions to hanle calculations

---
+++ utils/calculation.js
@@ -0,0 +1,21 @@
+export const calculateDays = (startDate, endDate) => {
+  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  const days = Math.round(Math.abs((startDate - endDate) / oneDay));
+  return days;
+};
+
+export const calculateRoomPrice = (rooms) => {
+  let sum = 0;
+
+  rooms.forEach((room) => {
+    const price = room.room.price;
+
+    sum += price;
+  });
+
+  return sum;
+};
+
+export const calculateSubtotal = (price, days) => {
+  return price * days;
+};
