f376cf1ed1ddbccc139e32eb73f0eefcb1f392df
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-13 19:45:58 +0200

    feat: updated page, fixed global props handeling

--- pages/enquiry.js
+++ pages/enquiry.js
@@ -1,29 +1,54 @@
 import Head from "../components/layout/Head";
 import Layout from "../components/layout/Layout";
 import Footer from "../components/Footer";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import BookingForm from "../components/BookingForm";
 import Link from "next/link";
-import EnquiryContext from "../context/EnquiryContext";
 import BookingInfo from "../components/BookingInfo";
+import {
+  calculateDays,
+  calculateRoomPrice,
+  calculateSubtotal,
+} from "../utils/calculation";
 
 const enquiry = () => {
-  const [data] = useContext(EnquiryContext);
-
-  useEffect(() => {}, []);
+  const [data, setData] = useState();
+  useEffect(() => {
+    const storageData = JSON.parse(localStorage.getItem("booked-data"));
+    if (storageData) {
+      setData(storageData);
+    }
+  }, []);
   return (
     <Layout>
       <Head title="Enquiry" />
       <main className="enquiry">
-        <section className="enquiry__hotel-info">
+        <div className="enquiry__top">
           <h1 className="enquiry__heading">Book Now</h1>
           <Link href="/">
             <a className="enquiry__link">Go Back</a>
           </Link>
-          {data ? <BookingInfo data={data} /> : null}
+        </div>
+
+        <section className="enquiry__hotel-info">
+          {data ? (
+            <BookingInfo
+              data={data}
+              calculateRoomPrice={calculateRoomPrice}
+              calculateDays={calculateDays}
+              calculateSubtotal={calculateSubtotal}
+            />
+          ) : null}
         </section>
         <section className="enquiry__contact-info">
-          <BookingForm />
+          {data ? (
+            <BookingForm
+              data={data}
+              calculateRoomPrice={calculateRoomPrice}
+              calculateDays={calculateDays}
+              calculateSubtotal={calculateSubtotal}
+            />
+          ) : null}
         </section>
       </main>
       <Footer />
