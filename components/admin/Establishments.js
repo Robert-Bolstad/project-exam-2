a72055e17a9f5c9beb79c07e0048e113d6ff872b
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-22 02:45:06 +0200

    feat: added new component

---
+++ components/admin/Establishments.js
@@ -0,0 +1,139 @@
+import Image from "next/image";
+import { BASE_URL } from "../../settings/api";
+import Link from "next/link";
+import doDelete from "../../utils/doDelete";
+const Establishments = ({ data }) => {
+  function openEnquiry(element, id) {
+    const establishment = element.parentElement.querySelector(
+      ".Establishments__establishment"
+    );
+    const btnImg = element.querySelector(".Establishments__btn-img");
+    establishment.classList.toggle("Establishments__establishment--open");
+    btnImg.classList.toggle("Establishments__btn-img--open");
+  }
+
+  return (
+    <div className="Establishments">
+      <div className="Establishments__top">
+        <h1 className="Establishments__heading">Establishments</h1>
+
+        <Link href="/addEstablishment">
+          <a className="Establishments__link">+ Add New</a>
+        </Link>
+      </div>
+
+      {data.map((data) => {
+        return (
+          <div key={data.id} className="Establishments__wrapper">
+            <button
+              className="Establishments__btn"
+              onClick={(event) => openEnquiry(event.currentTarget, data.id)}
+            >
+              <span className="Establishments__btn-text">{data.name}</span>
+
+              <img
+                src="/arrow.svg"
+                alt="arrow-icon"
+                className="Establishments__btn-img"
+              />
+            </button>
+            <div className="Establishments__establishment">
+              <div className="Establishments__img">
+                {console.log(data)}
+                <Image
+                  src={BASE_URL + data.image[0].url}
+                  width={data.image[0].width}
+                  height={data.image[0].height}
+                  alt="hotel image"
+                />
+              </div>
+              <div className="Establishments__general">
+                <p className="Establishments__info">
+                  Name:
+                  <span className="Establishments__info-detail">
+                    {" " + data.name}
+                  </span>
+                </p>
+                <p className="Establishments__info">
+                  Type:
+                  <span className="Establishments__info-detail">
+                    {" " + data.category}
+                  </span>
+                </p>
+              </div>
+              <div className="Establishments__contact">
+                <h3 className="Establishments__contact-heading">
+                  Contact Info
+                </h3>
+                <p className="Establishments__info">
+                  Address:
+                  <span className="Establishments__info-detail">
+                    {" " + data.address}
+                  </span>
+                </p>
+                <p className="Establishments__info">
+                  Email:
+                  <span className="Establishments__info-detail">
+                    {" " + data.email}
+                  </span>
+                </p>
+                <p className="Establishments__info">
+                  Phone:
+                  <span className="Establishments__info-detail">
+                    {" " + data.phone}
+                  </span>
+                </p>
+              </div>
+              <div className="Establishments__rooms">
+                <h3 className="Establishments__rooms-heading">Rooms</h3>
+                <ul className="Establishments__booking-rooms">
+                  {data.rooms.map((room) => {
+                    return (
+                      <li
+                        key={Math.random()}
+                        className="Establishments__booking-room"
+                      >
+                        {room.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <p className="Establishments__info">
+                  Address:
+                  <span className="Establishments__info-detail">
+                    {" " + data.address}
+                  </span>
+                </p>
+                <p className="Establishments__info">
+                  Email:
+                  <span className="Establishments__info-detail">
+                    {" " + data.email}
+                  </span>
+                </p>
+                <p className="Establishments__info">
+                  Phone:
+                  <span className="Establishments__info-detail">
+                    {" " + data.phone}
+                  </span>
+                </p>
+              </div>
+              <Link href={"/edit/" + data.id}>
+                <a className="Establishments__link">+ Edit</a>
+              </Link>
+              <button
+                type="button"
+                onClick={() => {
+                  doDelete(data);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Establishments;
