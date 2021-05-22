e9d473139b7f44339b200fc41df469367d5b2d38
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-22 02:50:23 +0200

    feat: added code to upload establishment

--- pages/addestablishment.js
+++ pages/addestablishment.js
@@ -4,382 +4,198 @@ import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
+import EstablishmentDetails from "../components/admin/addEstablishment/EstablishmentDetails";
+import AddAmenities from "../components/admin/addEstablishment/AddAmenities";
+import AddRooms from "../components/admin/addEstablishment/AddRooms";
 import { BASE_URL } from "../settings/api";
-import Image from "next/image";
-import AmenitiesList from "../components/addAmenities/AmenitiesList";
-import AmenitiesForm from "../components/addAmenities/AmenitiesForm";
+import axios from "axios";
 
 export default function addEstablishment() {
   const [auth] = useContext(AuthContext);
   const router = useRouter();
-  const [geometry, setGeometry] = useState(null);
-  const [highlights, setHighlights] = useState(null);
-  const [contact, setContact] = useState(null);
-  const [Amenities, setAmenities] = useState(null);
+  const [hotelImage, setHotelImage] = useState(null);
+  const [details, setDetails] = useState(null);
+  const [amenitiesList, setAmenitiesList] = useState(null);
   const [rooms, setRooms] = useState(null);
-  const [todos, setTodos] = useState([]);
-  const [mainAmenities, setMainAmenities] = useState([]);
-  const [familyAmenities, setFamilyAmenities] = useState([]);
-  const [areaAmenities, setAreaAmenities] = useState([]);
+  const [reload, setReload] = useState(false);
 
-  const LOCAL_STORAGE_KEY = "react-todos-list-todos";
+  const [submitting, setSubmiting] = useState(false);
 
-  function addMainAmenities(amenities) {
-    setMainAmenities([amenities, ...mainAmenities]);
+  useEffect(() => {
+    if (
+      hotelImage !== null &&
+      amenitiesList !== null &&
+      details !== null &&
+      rooms !== null &&
+      submitting === true
+    ) {
+      handleUpload();
+    } else {
+      setReload(true);
+    }
+  }, [hotelImage, details, amenitiesList, rooms, submitting]);
+
+  useEffect(() => {
+    if (
+      hotelImage !== null &&
+      amenitiesList !== null &&
+      details !== null &&
+      rooms !== null &&
+      submitting === true
+    ) {
+      handleUpload();
+    }
+  }, [reload]);
+
+  function getHotelImage(imgfile) {
+    setHotelImage(imgfile);
   }
-  function addFamilyAmenities(amenities) {
-    setFamilyAmenities([amenities, ...familyAmenities]);
-  }
-  function addAreaAmenities(amenities) {
-    setAreaAmenities([amenities, ...areaAmenities]);
+
+  function getDetails(details) {
+    setDetails(details);
   }
 
-  function removeMainAmenities(id) {
-    setMainAmenities(mainAmenities.filter((item) => item.id !== id));
+  function getAmenities(amenities) {
+    setAmenitiesList(amenities);
   }
 
-  function removeFamilyAmenities(id) {
-    setFamilyAmenities(familyAmenities.filter((item) => item.id !== id));
+  function getRooms(room) {
+    setRooms(room);
   }
-
-  function removeAreaAmenities(id) {
-    setAreaAmenities(areaAmenities.filter((item) => item.id !== id));
-  }
-
-  // useEffect(() => {
-  //   // fires when app component mounts to the DOM
-  //   const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-  //   if (storageTodos) {
-  //     setTodos(storageTodos);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   // fires when todos array gets updated
-  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  // }, [todos]);
-
-  const defaultImage = "/placeholder.jpg";
-
-  const [previewImage, setPreviewImage] = useState(defaultImage);
-  const [file, setFile] = useState();
   useEffect(() => {
     if (!auth) {
       router.push("/login");
     }
   });
 
-  useEffect(() => {
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setPreviewImage(reader.result);
-      };
-      reader.readAsDataURL(file);
-    } else {
-      setPreviewImage(defaultImage);
-    }
-  }, [file]);
+  const handleUpload = () => {
+    let main = [];
+    let families = [];
+    let area = [];
+    let highlights = [];
+    let prices = [];
 
-  const [submitting, setSubmitting] = useState(false);
-  const [success, setSuccess] = useState("");
-  const phoneRegExp =
-    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-  const schema = yup.object().shape({
-    accomodationName: yup
-      .string()
-      .required("* Please enter the name of the hotel")
-      .min(3, "* The name must be at least 3 characters"),
+    rooms.forEach((price) => {
+      prices.push(price.price);
+    });
+    amenitiesList[0].forEach((item) => {
+      main.push(item.amenitie);
+    });
+    amenitiesList[1].forEach((item) => {
+      families.push(item.amenitie);
+    });
+    amenitiesList[2].forEach((item) => {
+      area.push(item.amenitie);
+    });
+    const selected = amenitiesList[0].filter((item) => item.selected === true);
 
-    description: yup
-      .string()
-      .required("* Please enter an description of your accommodation")
-      .min(50, "* The description must be at least 50 characters"),
+    selected.forEach((item) => {
+      highlights.push(item.amenitie);
+    });
 
-    address: yup
-      .string()
-      .required("* Please enter the  full address of your location"),
+    const minPrice = Math.min(...prices);
 
-    category: yup
-      .string()
-      .oneOf(
-        ["hotel", "bed_and_breakfast"],
-        "* Please select select an accommodation type"
-      )
-      .required("* Please select your accommodation type"),
+    const amenities = {
+      amenities: { main: main, families: families, area: area },
+    };
+    const Accommodation_highlights = {
+      accommodation_highlights: {
+        highlights: highlights,
+      },
+    };
 
-    phone: yup
-      .string()
-      .required("* Please enter your phone number")
-      .matches(phoneRegExp, "* Invalid phone number."),
+    const geometry = {
+      geometry: {
+        latitude: Number(details.latitude),
+        longitude: Number(details.longitude),
+      },
+    };
 
-    email: yup
-      .string()
-      .required("* Please enter an email address")
-      .email("* Please enter a valid email address"),
+    const generalData = {
+      name: details.name,
+      description: details.description,
+      price: minPrice,
+      featured: details.featured,
+      category: details.category,
+      email: details.email,
+      phone: details.phone,
+      address: details.address,
+    };
 
-    subject: yup
-      .string()
-      .required("* Please enter your last name")
-      .min(4, "* Subject must be at least 4 characters"),
-  });
+    const roomData = { rooms: rooms };
 
-  function successHtml() {
-    return (
-      <>
-        <p>Message sendt</p>
-        <div className="contact-form__check-img">
-          <Image src="/check.svg" width="12" height="12" alt="check icon" />
-        </div>
-      </>
+    const data = Object.assign(
+      generalData,
+      amenities,
+      geometry,
+      Accommodation_highlights,
+      roomData
     );
-  }
 
-  const { register, handleSubmit, errors } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmit = async (data) => {
-    setSubmitting(true);
-
-    try {
-      const response = await axios.post(BASE_URL + "/establishment", data);
-      console.log(data);
-    } catch (error) {
-      console.log("error", error);
-    } finally {
-      setSubmitting(false);
-      setSuccess(successHtml());
-    }
+    axios
+      .post(BASE_URL + "/establishments", data)
+      .then((res) => {
+        console.log(res);
+        return res.data.id;
+      })
+      .then((refId) => {
+        const imageData = new FormData();
+        imageData.append("files", hotelImage);
+        imageData.append("refId", refId);
+        imageData.append("field", "image");
+        imageData.append("ref", "establishments");
+        return axios.post(BASE_URL + "/upload", imageData);
+      })
+      .then((res) => {
+        console.log(res);
+        alert("You successfully uploaded");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const imageFileHandler = (event) => {
-    setFile(event.target.files[0]);
-  };
+  const submitForm = () => {
+    const detailsForm = document.querySelector(".EstablishmentDetails__submit");
+    const amenitiesForm = document.querySelector(".AddAmenities__submit");
+    const roomsForm = document.querySelector(".AddRooms__submit-rooms");
 
-  const handleClick = () => {
-    document.querySelector(".AddEstablishment__addfile").click();
+    detailsForm.click();
+    amenitiesForm.click();
+    roomsForm.click();
+
+    setSubmiting(true);
   };
 
   return (
     <Layout>
       <Head title="Add Establishment" />
       <main className="AddEstablishment">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="AddEstablishment__form"
-        >
-          <div className="AddEstablishment__success">{success}</div>
-          <fieldset
-            className="AddEstablishment__fieldset"
-            disabled={submitting}
-          >
-            <div className="AddEstablishment__wrapper">
-              <div className="AddEstablishment__group">
-                <Image
-                  src={previewImage}
-                  width="500px"
-                  height="400px"
-                  alt="Display image"
-                />
-                <button
-                  onClick={handleClick}
-                  className="AddEstablishment__addfile-btn"
-                >
-                  Choose image
-                </button>
-                <input
-                  className="AddEstablishment__addfile"
-                  type="file"
-                  onChange={imageFileHandler}
-                />
-              </div>
+        <div className="AddEstablishment__wrapper">
+          <h1 className="AddEstablishment__heading">Add Establishment</h1>
+          <section className="AddEstablishment__section">
+            <EstablishmentDetails
+              getHotelImage={getHotelImage}
+              getDetails={getDetails}
+            />
+          </section>
+          <section className="AddEstablishment__AddAmenities">
+            <AddAmenities getAmenities={getAmenities} />
+          </section>
+          <section className="AddEstablishment__section">
+            <AddRooms getRooms={getRooms} />
+          </section>
 
-              <div className="AddEstablishment__group">
-                <label
-                  className="AddEstablishment__label"
-                  htmlFor="AddEstablishment__input--name"
-                >
-                  Accomodation name
-                </label>
-                <input
-                  ref={register}
-                  className="AddEstablishment__input AddEstablishment__input--name"
-                  type="text"
-                  name="accomodationName"
-                  id="AddEstablishment__input--name"
-                />
-                {errors.accomodationName && (
-                  <span className="AddEstablishment__error-message">
-                    {errors.accomodationName.message}
-                  </span>
-                )}
-              </div>
-              <div className="AddEstablishment__group">
-                <label
-                  className="AddEstablishment__label"
-                  htmlFor="AddEstablishment__email"
-                >
-                  Email
-                </label>
-                <input
-                  className="AddEstablishment__email AddEstablishment__input"
-                  name="email"
-                  ref={register}
-                  type="text"
-                />
-                {errors.email && (
-                  <span className="AddEstablishment__error-message">
-                    {errors.email.message}
-                  </span>
-                )}
-              </div>
-
-              <div className="AddEstablishment__group">
-                <label
-                  className="AddEstablishment__label"
-                  htmlFor="AddEstablishment__phone"
-                >
-                  Phone
-                </label>
-                <input
-                  className="AddEstablishment__phone AddEstablishment__input"
-                  name="phone"
-                  ref={register}
-                  type="text"
-                />
-                {errors.phone && (
-                  <span className="AddEstablishment__error-message">
-                    {errors.phone.message}
-                  </span>
-                )}
-              </div>
-
-              <div className="AddEstablishment__group">
-                <label
-                  className="AddEstablishment__label"
-                  htmlFor="AddEstablishment__phone"
-                >
-                  Address
-                </label>
-                <input
-                  className="AddEstablishment__address AddEstablishment__input"
-                  name="address"
-                  ref={register}
-                  type="text"
-                />
-                {errors.address && (
-                  <span className="AddEstablishment__error-message">
-                    {errors.address.message}
-                  </span>
-                )}
-              </div>
-
-              <div className="AddEstablishment__group">
-                <label
-                  className="AddEstablishment__label"
-                  htmlFor="AddEstablishment__input--textbox"
-                >
-                  Description
-                </label>
-                <textarea
-                  className="AddEstablishment__textbox"
-                  name="description"
-                  ref={register}
-                  type="text"
-                  id="AddEstablishment__input--textbox"
-                />
-                {errors.description && (
-                  <span className="AddEstablishment-message">
-                    {errors.description.message}
-                  </span>
-                )}
-              </div>
-
-              <div className="AddEstablishment__group">
-                <label
-                  className="AddEstablishment__label"
-                  htmlFor="AddEstablishment__select"
-                >
-                  Accommodation Type
-                </label>
-                <select
-                  name="category"
-                  ref={register}
-                  className="AddEstablishment__select"
-                  type="text"
-                >
-                  <option style={{ display: "none" }} value="">
-                    Select your type
-                  </option>
-                  <option value="hotel">Hotel</option>
-                  <option value="bed_and_breakfast">Bed and breakfast</option>
-                  <option value="guesthouse">Subject Two</option>
-                </select>
-                {errors.category && (
-                  <span className="AddEstablishment__error-message">
-                    {errors.category.message}
-                  </span>
-                )}
-              </div>
-              <div className="AddEstablishment__group">
-                <h3>Add Amenities</h3>
-                <p>Add amenities you want to display on your page.</p>
-
-                <div>
-                  <h4>Main Amenities</h4>
-                  <p>
-                    Add at least 3 Main amenities you would like to add to your
-                    page. your first entries three will be highlighted at the
-                    top of the page
-                  </p>
-                  <AmenitiesForm
-                    addItem={addMainAmenities}
-                    placeholderText="Free Wifi, Parking Included, Breakfast included."
-                  />
-                  <AmenitiesList
-                    amenities={mainAmenities}
-                    removeItem={removeMainAmenities}
-                  />
-                </div>
-                <div>
-                  <h4>For Families</h4>
-                  <p>Add at least 3 amenities for families</p>
-                  <AmenitiesForm
-                    addItem={addFamilyAmenities}
-                    placeholderText="Television, Daily housekeeping, Laundry facilities etc."
-                  />
-                  <AmenitiesList
-                    amenities={familyAmenities}
-                    removeItem={removeFamilyAmenities}
-                  />
-                </div>
-                <div>
-                  <h4>Amenities in the area</h4>
-                  <p>Add at least 3 amenities thats in the area</p>
-                  <AmenitiesForm
-                    addItem={addAreaAmenities}
-                    placeholderText="Parks, Shops, Malls, Hiking area etc."
-                  />
-                  <AmenitiesList
-                    amenities={areaAmenities}
-                    removeItem={removeAreaAmenities}
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="AddEstablishment__group">
-              <button type="submit" className="AddEstablishment__submit">
-                Submit
-              </button>
-            </div>
-          </fieldset>
-        </form>
+          <div className="AddEstablishment__btn-wrapper">
+            <button
+              className="AddEstablishment__btn"
+              type="button"
+              onClick={submitForm}
+            >
+              Add Establishment
+            </button>
+          </div>
+        </div>
       </main>
     </Layout>
   );
