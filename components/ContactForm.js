a3b16d2b81beb31c4f80ca155236c87389df770d
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-22 02:43:23 +0200

    feat: moved yup code to another folder

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,40 +1,14 @@
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useState } from "react";
 import { BASE_URL } from "../settings/api";
 import Image from "next/image";
+import { schema } from "../utils/schemaValidators/contactFormValidators";
 
 const ContactForm = () => {
   const [submitting, setSubmitting] = useState(false);
   const [success, setSuccess] = useState("");
-  const schema = yup.object().shape({
-    firstName: yup
-      .string()
-      .required("* Please enter your first name")
-      .min(3, "* First Name must be at least 3 characters"),
-
-    lastName: yup
-      .string()
-      .required("* Please enter your last name")
-      .min(4, "* Last Name must be at least 4 characters"),
-
-    email: yup
-      .string()
-      .required("* Please enter an email address")
-      .email("* Please enter a valid email address"),
-
-    subject: yup
-      .string()
-      .required("* Please enter your last name")
-      .min(4, "* Subject must be at least 4 characters"),
-
-    message: yup
-      .string()
-      .required("* Please enter an message")
-      .min(10, "* The message must be at least 10 characters"),
-  });
 
   function successHtml() {
     return (
