407e9a39ceac0566a6647c4fb5f7987f1beb3555
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-23 20:17:33 +0200

    feat: updated function to delete files

--- utils/doDelete.js
+++ utils/doDelete.js
@@ -4,8 +4,7 @@ import { BASE_URL } from "../settings/api";
 const doDelete = async (data) => {
   const id = data.id;
   const name = data.name;
-
-  // Creating a confirm message
+  const imageId = data.image[0].id;
   const confirmDelete = confirm(
     "Are you sure you want to delete " + name + " from your list?"
   );
@@ -13,6 +12,7 @@ const doDelete = async (data) => {
   if (confirmDelete) {
     try {
       await axios.delete(BASE_URL + "/establishments/" + id);
+      await axios.delete(BASE_URL + "/upload/files/" + imageId);
     } catch (error) {
       console.log("error", error);
     } finally {
