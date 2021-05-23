06a47c47328eb659febcc5c6ad554c8ee2e46fa5
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-05-23 20:17:50 +0200

    fix: cleaned up code

--- settings/api.js
+++ settings/api.js
@@ -1,5 +1,3 @@
-//export const BASE_URL = "http://localhost:9999";
-//export const AUTH_PATH = "http://localhost:9999/auth/local";
 export const BASE_URL = "https://boiling-harbor-97485.herokuapp.com";
 export const AUTH_PATH =
   "https://boiling-harbor-97485.herokuapp.com/auth/local";
